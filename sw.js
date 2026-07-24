b17f901fa0f3c9223120a82fa20fa7a44875616e
lerma-dev <alejandrolerma256@gmail.com>
2026-07-24 17:32:40 -0600

    Se mejoran los iconos y se mejoro el responsive para mobile

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('calc-v1').then((cache) => cache.addAll([
+    caches.open('calculadora-v2').then((cache) => cache.addAll([
       './',
       './index.html',
-      './style.css',
-      './script.js'
+      './manifest.json',
+      './assets/style.css',
+      './assets/icons/favicon.svg',
+      './assets/icons/icon-192.svg',
+      './assets/icons/icon-512.svg',
+      './assets/icons/icon-180.svg',
+      './assets/script.js'
     ]))
   );
 });
